27cb358f42170364701e8921c665ca65f3243d02
sleshword <mihail.troshin@maximumtest.ru>
2023-11-14 18:59:16 +0300

    first commit

---
+++ OMP/1_lab/omp_mesh3d.f90
@@ -0,0 +1,50 @@
+PROGRAM OPENMP_3D_ARRAY
+    INCLUDE 'omp_lib.h'
+    INTEGER I, J, K, L, NI, NJ, NK, INFILE, OUTFILE
+    DOUBLE PRECISION T1, T2
+    DOUBLE PRECISION, ALLOCATABLE :: X(:,:,:), Y(:,:,:), Z(:,:,:)
+
+    OPEN(INFILE, FILE="CUBE.MSH")
+    READ(INFILE,*) NI, NJ, NK 
+    ALLOCATE (X(2*NI,2*NJ,2*NK), Y(2*NI,2*NJ,2*NK), Z(2*NI,2*NJ,2*NK))
+    DO K=(NK/2 + 1), NK 
+      DO J=(NJ/2 + 1), NJ
+       DO I=(NI/2 + 1), NI 
+        READ(INFILE,*) X(I,J,K), Y(I,J,K), Z(I,J,K) 
+       END DO 
+      END DO
+    END DO
+    CLOSE(INFILE)
+
+    T1 = OMP_GET_WTIME()
+!$OMP PARALLEL PRIVATE(I,J,K)
+    DO L=1,300
+!$OMP DO
+    DO K=1, (NK/2)
+     DO J=1, (NJ/2)
+      DO I=1, (NI/2)
+          X(I,J,K) = X((N/2 +I),(N/2 +J),(N/2 +K))
+          Y(I,J,K) = -Y((N/2 +I),(N/2 +J),(N/2 +K))
+          Z(I,J,K) = -Z((N/2 +I),(N/2 +J),(N/2 +K))
+      END DO
+     END DO
+    END DO
+!$OMP END DO
+    END DO
+!$OMP END PARALLEL
+    T2 = OMP_GET_WTIME()
+    PRINT *, 'computational time: ', T2-T1, 's'
+
+    OPEN(OUTFILE, FILE="CUBE_NEW.MSH")
+    WRITE(OUTFILE,*) NI, NJ, NK 
+    DO K=1, 2*NK 
+      DO J=1, 2*NJ 
+       DO I=1, 2*NI 
+        WRITE(OUTFILE,*) X(I,J,K), Y(I,J,K), Z(I,J,K) 
+       END DO 
+      END DO
+    END DO
+    CLOSE(OUTFILE)
+    DEALLOCATE(X,Y,Z)
+
+END
